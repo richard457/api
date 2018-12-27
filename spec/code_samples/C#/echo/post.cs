ba04892a745f859bb72f38af0f8459b031db2f48
Muragijimana <beastar457@gmail.com>
2018-12-27 15:18:27 +0200

    no code sample

--- spec/code_samples/C#/echo/post.cs
+++ spec/code_samples/C#/echo/post.cs
@@ -1,12 +1,12 @@
-API.v1.Echo echo = new API.v1.Echo();
-echo.message = "Hello World!");
-EchoResponse response = echo.post();
-if (response.statusCode == HttpStatusCode.Created)
-{
-  // Success
-}
-else
-{
-  // Something wrong -- check response for errors
-  Console.WriteLine(response.getRawResponse());
-}
+// API.v1.Echo echo = new API.v1.Echo();
+// echo.message = "Hello World!");
+// EchoResponse response = echo.post();
+// if (response.statusCode == HttpStatusCode.Created)
+// {
+//   // Success
+// }
+// else
+// {
+//   // Something wrong -- check response for errors
+//   Console.WriteLine(response.getRawResponse());
+// }
